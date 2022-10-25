bc84c5fb7d16fdf73b3c99755747e0bb01c6688b
Pablo Pedreros <p.pedreros@uniandes.edu.co>
2022-10-25 22:55:33 +0000

    Inicial

---
+++ Proyecto_guiado/main.ts
@@ -0,0 +1,5 @@
+import {Aprendiz, NivelEducativo} from './aprendiz.js';
+
+export const ap = new Aprendiz("Juan Pablo", "Reyes Gomez", "avatar.png",30,NivelEducativo.POSGRADO);
+
+console.log(ap);
---
+++ Proyecto_guiado/scripts/main.js
@@ -0,0 +1,3 @@
+import { Aprendiz, NivelEducativo } from './aprendiz.js';
+export var ap = new Aprendiz("Juan Pablo", "Reyes Gomez", "avatar.png", 30, NivelEducativo.POSGRADO);
+console.log(ap);
